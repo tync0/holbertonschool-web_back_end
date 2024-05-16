e188aaf2480df9dd600756832cb9dd020cada8de
tync0 <isgenderlituncay2004@gmail.com>
2024-05-16 21:08:59 +0400

    Task 4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -3,12 +3,6 @@ import Currency from "./3-currency";
 
 export default class Pricing {
     constructor(amount, currency) {
-        if (typeof amount !== 'number') {
-            throw new TypeError('Amount must be a number');
-        }
-        if (typeof currency !== 'Currency') {
-            throw new TypeError('Currency must be a currency');
-        }
         this._amount = amount;
         this._currency = currency;
     }
@@ -20,16 +14,10 @@ export default class Pricing {
         return this._currency;
     }
     set amount(amount){
-        if (typeof amount !== 'number') {
-            throw new TypeError('Amount must be a number');
-        }
         this._amount = amount;
     }
 
     set currency(currency){
-        if (typeof currency !== 'currency') {
-            throw new TypeError('Currency must be a currency');
-        }
         this._currency = currency;
     }
     
