5aaf52bc2f252a5c973a7cbc01df5f802ae10d47
tync0 <isgenderlituncay2004@gmail.com>
2024-05-16 17:35:18 +0400

    Task 1

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,8 +1,5 @@
 import ClassRoom from "./0-classroom";
 
-export function initializeRooms() {
-    const room1 = new ClassRoom(19);
-    const room2 = new ClassRoom(20);
-    const room3 = new ClassRoom(34);
-    return [room1, room2, room3];
+export default function initializeRooms() {
+    return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
