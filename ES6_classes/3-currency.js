f465a8b23d1bb1fd2b5097ca1c64cde3d8731457
tync0 <isgenderlituncay2004@gmail.com>
2024-05-16 21:08:54 +0400

    Task 3

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,12 +1,6 @@
 /* eslint-disable */
 export default class Currency {
     constructor(name, code) {
-        if (typeof name !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
-        if (typeof code !== 'string') {
-            throw new TypeError('Code must be a string');
-        }
         this._name = name;
         this._code = code;
     }
@@ -18,16 +12,10 @@ export default class Currency {
         return this._code;
     }
     set name(name){
-        if (typeof name !== 'string') {
-            throw new TypeError('Name must be a string');
-        }
         this._name = name;
     }
 
     set code(code){
-        if (typeof code !== 'string') {
-            throw new TypeError('Code must be a string');
-        }
         this._code = code;
     }
     
