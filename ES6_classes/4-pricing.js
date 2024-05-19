2d65e558b8dbeeffb79e7a7f86f598a7ff0fdace
tync0 <isgenderlituncay2004@gmail.com>
2024-05-19 18:57:06 +0400

    Task 4

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,36 +1,37 @@
 /* eslint-disable */
-import Currency from "./3-currency";
+import Currency from './3-currency';
 
 export default class Pricing {
-    constructor(amount, currency) {
-        this._amount = amount;
-        this._currency = currency;
-    }
-    get amount() {
-        return this._amount;
-    }
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
 
-    get currency() {
-        return this._currency;
-    }
+  get amount() {
+    return this._amount;
+  }
 
-    set amount(newAmount) {
-        if (typeof newAmount === 'number') {
-          this._amount = newAmount;
-        }
-    }
-    
-    set currency(newCurrency) {
-        if (newCurrency instanceof Currency) {
-          this._currency = newCurrency;
-        }
-    }
-    
-    displayFullPrice(){
-        return `${this.amount} ${this.currency.name} (${this.currency.code})`;
-    }
+  get currency() {
+    return this._currency;
+  }
 
-    static convertPrice(amount, conversionRates){
-        return amount * conversionRates;
+  set amount(newAmount) {
+    if (typeof newAmount === 'number') {
+      this._amount = newAmount;
     }
+  }
+
+  set currency(newCurrency) {
+    if (newCurrency instanceof Currency) {
+      this._currency = newCurrency;
+    }
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
 }
